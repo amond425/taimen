bf57d12208e6cccc3fecebce3c9070e45c313cc4
unknown <admin@skm.vn>
2023-03-11 19:34:47 +0700

    Update

---
+++ assets/js/analytics_v1_2.js
@@ -0,0 +1,30 @@
+function loadGoogleAnalytics(blocked) {
+    if (blocked) {
+        console.log("[AD Disabled]");
+    } else {
+        document.title= "[AD] "+ document.title;
+        console.log("[AD]");
+    }
+    
+    // Google tag (gtag.js)
+    var f=document.getElementsByTagName("script")[0];
+    j=document.createElement("script");
+    j.async="";  
+    j.src= "https://www.googletagmanager.com/gtag/js?id=G-E7D3EVY6HR";
+    f.parentNode.insertBefore(j,f);
+
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+    gtag('config', 'G-E7D3EVY6HR');
+}
+
+$(document).ready(function() {
+    $.getScript( "https://www.ubg235.com/js/analytics.js" )
+        .done(function( script, textStatus ) {            
+            loadGoogleAnalytics(false);
+        })
+        .fail(function( jqxhr, settings, exception ) {
+            loadGoogleAnalytics(true);
+        });
+});
---
+++ assets/js/redirect_v1_3.js
@@ -0,0 +1,31 @@
+function redirectUrl(domain, path) {
+    return "https://"+ domain+ "/"+ path;
+}
+
+function redirectLoad(blocked) {
+    if (blocked) {
+        console.log("[RD Disabled]");
+    } else {        
+        console.log("[RD]");
+        if (typeof ADVERTISEMENT_REDIRECT === 'undefined' || ADVERTISEMENT_REDIRECT === null) {
+            console.log("[RD No Option]");
+            return false;
+        }
+        for (const [dom, domain] of Object.entries(ADVERTISEMENT_REDIRECT)) {
+            console.log(`${dom} => ${domain}`);
+            $(`.${dom}`).each(function() {
+                $(this).attr("href", redirectUrl(domain, $(this).attr("href")));
+            });
+        }
+    }
+}
+
+$(document).ready(function() {
+    $.getScript( "https://www.ubg235.com/js/ads.js?ws=ubg235.github.io" )
+        .done(function( script, textStatus ) {            
+            redirectLoad(false);
+        })
+        .fail(function( jqxhr, settings, exception ) {
+            redirectLoad(true);
+        });
+});
